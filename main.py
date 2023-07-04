483099fb98c337e9472083a5526dacb6109900c1
yishen.cai <yishen.cai@qunar.com>
2023-07-04 14:29:46 +0800

    调整日志

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.DEBUG)
 parser = argparse.ArgumentParser()
 parser.add_argument("-log", "--log", nargs='+', help="Provide logging level. Example --log debug'")
 log_level = parser.parse_args().log
-log_level = log_level[0] if len(log_level) > 0 else logging.INFO
+log_level = log_level[0] if log_level is not None and len(log_level) > 0 else logging.INFO
 logger.setLevel(log_level)
 
 if __name__ == '__main__':
